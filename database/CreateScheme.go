5e1b55cb9d3e433faf1bed9cf7fcd87dc29d666d
matscus <matscus@yandex.ru>
2020-08-04 15:07:31 +0000

    change pguser from os env

--- database/CreateScheme.go
+++ database/CreateScheme.go
@@ -1,10 +1,13 @@
 package database
 
-import "errors"
+import (
+	"errors"
+	"os"
+)
 
 //CreateScheme - create scheme and table from pool
 func CreateScheme(scheme string) error {
-	_, err := DB.Exec("CREATE SCHEMA IF NOT EXISTS " + scheme + " AUTHORIZATION postgres")
+	_, err := DB.Exec("CREATE SCHEMA IF NOT EXISTS " + scheme + " AUTHORIZATION " + os.Getenv("POSTGRESUSER"))
 	if err != nil {
 		return errors.New("Create scheme error: " + err.Error())
 	}
