68f3e06ffc657db3fb6f97211e133800406c1460
Роман Евгеньевич Мацкус <REMatskus@sberbank.ru>
2021-08-18 14:45:29 +0300

    rename pakage metrics

--- middleware/Middleware.go
+++ middleware/Middleware.go
@@ -4,20 +4,21 @@ import (
 	"net/http"
 	"time"
 
-	"github.com/matscus/ammunition/monitoring"
+	"github.com/matscus/ammunition/metrics"
 	log "github.com/sirupsen/logrus"
 )
 
 func Middleware(f http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
-		monitoring.RequestCount.Inc()
+		metrics.RequestCount.Inc()
 		start := time.Now()
 		defer func() {
-			if r := recover(); r != nil {
-				log.Debug("Recovered")
+			if err := recover(); err != nil {
+				log.Error("Middleware recover panic ", err)
 			}
-			monitoring.RequestsDuration.WithLabelValues(r.Method, r.URL.Path).Observe(float64(time.Since(start).Milliseconds()))
+			metrics.RequestsDuration.WithLabelValues(r.Method, r.URL.Path).Observe(float64(time.Since(start).Milliseconds()))
 		}()
+		f(w, r)
 	}
 }
