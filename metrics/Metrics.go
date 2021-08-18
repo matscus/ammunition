2f7bb8e59b64062e5df18a4ee62c62f7ecc82206
Роман Евгеньевич Мацкус <REMatskus@sberbank.ru>
2021-08-18 14:45:03 +0300

    change init and add workers metrics

---
+++ metrics/Metrics.go
@@ -0,0 +1,66 @@
+package metrics
+
+import (
+	"github.com/prometheus/client_golang/prometheus"
+	log "github.com/sirupsen/logrus"
+)
+
+var (
+	RequestCount = prometheus.NewCounter(prometheus.CounterOpts{
+		Name: "ammunition_http_requests_count_total",
+		Help: "The total number of request events",
+	})
+	RequestsDuration = prometheus.NewSummaryVec(
+		prometheus.SummaryOpts{
+			Name:       "ammunition_http_requests_duration_ms",
+			Help:       "A summary of the handling duration of requests.",
+			Objectives: map[float64]float64{0.9: 0.01, 0.95: 0.01, 0.99: 0.01},
+		},
+		[]string{"method", "path"},
+	)
+	WorkerCount = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "ammunition_worker_count_total",
+			Help: "Total number of workers",
+		},
+		[]string{"cache"},
+	)
+	WorkerDuration = prometheus.NewSummaryVec(
+		prometheus.SummaryOpts{
+			Name:       "ammunition_worker_duration_ms",
+			Help:       "A summary of the duration of worker.",
+			Objectives: map[float64]float64{0.9: 0.01, 0.95: 0.01, 0.99: 0.01},
+		},
+		[]string{"cache"},
+	)
+	CacheCount = prometheus.NewGauge(
+		prometheus.GaugeOpts{
+			Name: "ammunition_cache_count",
+			Help: "cache count",
+		},
+	)
+	CacheLen = prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Name: "ammunition_cache_len_total",
+			Help: "cache len",
+		},
+		[]string{"cache_name"},
+	)
+	Uptime = prometheus.NewGauge(
+		prometheus.GaugeOpts{
+			Name: "ammunition_uptime",
+			Help: "ammunition uptime.",
+		},
+	)
+)
+
+func init() {
+	prometheus.MustRegister(RequestCount)
+	prometheus.MustRegister(RequestsDuration)
+	prometheus.MustRegister(WorkerDuration)
+	prometheus.MustRegister(WorkerCount)
+	prometheus.MustRegister(CacheCount)
+	prometheus.MustRegister(CacheLen)
+	prometheus.MustRegister(Uptime)
+	log.Info("Register metrics completed")
+}
