28b90a475628c858f03a020b3c416b032bd3efeb
Роман Евгеньевич Мацкус <REMatskus@sberbank.ru>
2021-11-24 14:11:55 +0300

    remove depricated struct

--- cache/Common.go
+++ cache/Common.go
@@ -15,19 +15,3 @@ type Cache struct {
 	Life         time.Duration
 	Clean        time.Duration
 }
-
-type Data struct {
-	Key   string `json:"key,omitempty"`
-	Value string `json:"value,omitempty"`
-}
-
-type Cookies struct {
-	Key      string     `json:"key,omitempty"`
-	Platform IAMCookies `json:"platform"`
-	UFS      string     `json:"ufs"`
-}
-
-type IAMCookies struct {
-	PlatformSession  string `json:"platformSession,omitempty"`
-	PlatformSession2 string `json:"platformSession2,omitempty"`
-}
